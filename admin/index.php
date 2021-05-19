4e2340dcf96eef1502e42f23ea2db2fa7a7201e1
Ashinindavudha <ashinindavudha2018@gmail.com>
2021-05-19 12:21:50 +0630

    first_init

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<?php include("nav/head.php") ?>
+
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="rose" data-background-color="black" data-image="../assets/img/sidebar-1.jpg">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+      <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-mini">
+          CT
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          Creative Tim
+        </a></div>
+     <?php include("nav/sidebar.php") ?>
+    </div>
+    <div class="main-panel">
+      <!-- Navbar -->
+     <?php include("nav/navbar.php") ?>
+      <!-- End Navbar -->
+      <div class="content">
+     
+      </div>
+      <?php include("nav/footer.php") ?>
+    </div>
+  </div>
+  
+  
+  <?php include("nav/RightSidebar.php") ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'dhammavira/php-basic-project',
+  ),
+  'versions' => 
+  array (
+    'dhammavira/php-basic-project' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'fakerphp/faker' => 
+    array (
+      'pretty_version' => 'v1.14.1',
+      'version' => '1.14.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ed22aee8d17c7b396f74a58b1e7fefa4f90d5ef1',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8622567409010282b7aeebe4bb841fe98b58dcaf',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.4.0',
+      'version' => '2.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f38c8804a9e97d23e0c8d63341088cd8a22d627',
+    ),
+  ),
+);
