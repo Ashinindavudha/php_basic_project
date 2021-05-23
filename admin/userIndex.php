5621bdb9769721ceabac755eff3190ea5f521a34
Ashinindavudha <ashinindavudha2018@gmail.com>
2021-05-23 09:37:19 +0630

    fifth_init

--- admin/userIndex.php
+++ admin/userIndex.php
@@ -12,53 +12,80 @@ $all = $table->getAll();
 $auth = Auth::check();
 
 ?>
+<?php include("nav/head.php") ?>
+<?php
+    // session_start();
+    // if (!isset($_SESSION['user'])) {
+    //   header('location: index.php');
+    //   exit();
+    // }
+  ?>
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="rose" data-background-color="black" data-image="../assets/img/sidebar-1.jpg">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Manage Users</title>
-
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- CSS Files -->
-  <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
-</head>
-
-<body>
-	<div class="container">
-		<div style="float: right">
-			<a href="profile.php">Profile</a> |
-			<a href="_actions/logout.php" class="text-danger">Logout</a>
-		</div>
-
-		<h1 class="mt-5 mb-5">
-			Manage Users
-			<span class="badge bg-danger text-white">
-				<?= count($all) ?>
-			</span>
-		</h1>
-
-		<table class="table table-striped table-bordered">
-			<tr>
-				<th>ID</th>
-				<th>Name</th>
-				<th>Email</th>
-				<th>Phone</th>
-				<th>Role</th>
-				<th>Actions</th>
-			</tr>
-			<?php foreach ($all as $user) : ?>
-				<tr>
-					<td><?= $user->id ?></td>
-					<td><?= $user->name ?></td>
-					<td><?= $user->email ?></td>
-					<td><?= $user->phone ?></td>
-					<td>
+        Tip 2: you can also add an image using data-image tag
+    -->
+      <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-mini">
+          CT
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          Creative Tim
+        </a></div>
+     <?php include("nav/sidebar.php") ?>
+    </div>
+    <div class="main-panel">
+      <!-- Navbar -->
+     <?php include("nav/navbar.php") ?>
+      <!-- End Navbar -->
+      <div class="content">
+		<!-- datatable start -->
+		 <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">assignment</i>
+                  </div>
+                  <h4 class="card-title">User DataTables</h4>
+                </div>
+                <div class="card-body">
+                  <div class="toolbar">
+                    <!--        Here you can write extra buttons/actions for the toolbar              -->
+                  </div>
+                  <div class="material-datatables">
+                    <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                      <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Name</th>
+                          <th>Email</th>
+                          <th>Phone</th>
+                          <th>Role</th>
+                          <th class="disabled-sorting text-right">Actions</th>
+                        </tr>
+                      </thead>
+                      <tfoot>
+                        <tr>
+                          <th>ID</th>
+                          <th>Name</th>
+                          <th>Email</th>
+                          <th>Phone</th>
+                          <th>Role</th>
+                          <th class="text-right">Actions</th>
+                        </tr>
+                      </tfoot>
+                      <tbody>
+                       <?php foreach ($all as $user) : ?>
+                        <tr>
+                          <td><?= $user->id ?></td>
+                          <td><?= $user->name ?></td>
+                          <td><?= $user->email ?></td>
+                         <td><?= $user->phone ?></td>
+                         <td>
 						<?php if ($user->value === '1') : ?>
 							<span class="badge bg-secondary">
 								<?= $user->role ?>
@@ -73,38 +100,70 @@ $auth = Auth::check();
 							</span>
 						<?php endif ?>
 					</td>
-					<td>
-						<?php if ($auth->value > 1) : ?>
-							<div class="btn-group dropdown">
-								<a href="#" class="btn btn-sm btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown">
-									Change Role
-								</a>
-								<div class="dropdown-menu dropdown-menu-dark">
-									<a href="_actions/role.php?id=<?= $user->id ?>&role=1" class="dropdown-item">User</a>
-									<a href="_actions/role.php?id=<?= $user->id ?>&role=2" class="dropdown-item">Manager</a>
-									<a href="_actions/role.php?id=<?= $user->id ?>&role=3" class="dropdown-item">Admin</a>
-								</div>
 
-								<?php if ($user->suspended) : ?>
-									<a href="_actions/unsuspend.php?id=<?= $user->id ?>" class="btn btn-sm btn-danger">Suspended</a>
-								<?php else : ?>
-									<a href="_actions/suspend.php?id=<?= $user->id ?>" class="btn btn-sm btn-outline-success">Active</a>
-								<?php endif ?>
+                          <td class="text-right">
+                            <a href="#" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>
+                            <a href="#" class="btn btn-link btn-warning btn-just-icon edit"><i class="material-icons">dvr</i></a>
+                            <a href="#" class="btn btn-link btn-danger btn-just-icon remove"><i class="material-icons">close</i></a>
+                          </td>
+                        </tr>
+						<?php endforeach ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <!-- end content-->
+              </div>
+              <!--  end card  -->
+            </div>
+            <!-- end col-md-12 -->
+          </div>
+          <!-- end row -->
+        </div>
+		<!-- datatable end -->
+      </div>
 
-								<?php if ($user->id !== $auth->id) : ?>
-									<a href="_actions/delete.php?id=<?= $user->id ?>" class="btn btn-sm btn-outline-danger" onClick="return confirm('Are you sure?')">Delete</a>
-								<?php endif ?>
-							</div>
-						<?php else : ?>
-							###
-						<?php endif ?>
-					</td>
-				</tr>
-			<?php endforeach ?>
-		</table>
-	</div>
+      <?php include("nav/footer.php") ?>
+    </div>
+  </div>
+  
+  
+  <?php include("nav/RightSidebar.php") ?>
 
-	<script src="js/bootstrap.bundle.min.js"></script>
-</body>
+  <script>
+    $(document).ready(function() {
+      $('#datatables').DataTable({
+        "pagingType": "full_numbers",
+        "lengthMenu": [
+          [10, 25, 50, -1],
+          [10, 25, 50, "All"]
+        ],
+        responsive: true,
+        language: {
+          search: "_INPUT_",
+          searchPlaceholder: "Search User records",
+        }
+      });
 
-</html>
+      var table = $('#datatable').DataTable();
+
+      // Edit record
+      table.on('click', '.edit', function() {
+        $tr = $(this).closest('tr');
+        var data = table.row($tr).data();
+        alert('You press on Row: ' + data[0] + ' ' + data[1] + ' ' + data[2] + '\'s row.');
+      });
+
+      // Delete a record
+      table.on('click', '.remove', function(e) {
+        $tr = $(this).closest('tr');
+        table.row($tr).remove().draw();
+        e.preventDefault();
+      });
+
+      //Like record
+      table.on('click', '.like', function() {
+        alert('You clicked on Like button');
+      });
+    });
+  </script>
